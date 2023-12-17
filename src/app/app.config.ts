5403003d64e08abcbfd1ee9085db56f4ba273b2a
Marcos Lopez <marcoslopezdev18@gmail.com>
2023-12-17 14:45:23 -0300

    successful config for firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { initializeApp } from "firebase/app";
-import { environment } from 'environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
-
-
-// Initialize Firebase
-const app = initializeApp(environment.firebaseConfig);
